162a9005e7e63d673bc84217ee2e425f084d9f4e
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-21 20:35:58 +0000

    User Update

--- src/main/java/com/example/springBootDTO/model/User.java
+++ src/main/java/com/example/springBootDTO/model/User.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class User {
 }
