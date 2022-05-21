4332fa03fda57daba6e509955ce619ec6cb6ecbf
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-21 21:07:06 +0000

    User Repository

--- src/main/java/com/example/springBootDTO/repositories/UserRepository.java
+++ src/main/java/com/example/springBootDTO/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.springBootDTO.repositories;
 
+import com.example.springBootDTO.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User> {
