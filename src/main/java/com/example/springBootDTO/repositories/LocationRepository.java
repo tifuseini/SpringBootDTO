e059d427b2a3efbdc7e0e62ff4da488e76e44e45
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-21 21:09:34 +0000

    Location  Repository

--- src/main/java/com/example/springBootDTO/repositories/LocationRepository.java
+++ src/main/java/com/example/springBootDTO/repositories/LocationRepository.java
@@ -3,5 +3,5 @@ package com.example.springBootDTO.repositories;
 import com.example.springBootDTO.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LocationRepository extends JpaRepository<Location> {
+public interface LocationRepository extends JpaRepository<Location,Long> {
 }
