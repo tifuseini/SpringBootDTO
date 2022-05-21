dd9b5c35e7d875aa9be4307633ced0e2a9841783
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-21 21:00:09 +0000

    Location

--- src/main/java/com/example/springBootDTO/model/Location.java
+++ src/main/java/com/example/springBootDTO/model/Location.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -16,6 +17,6 @@ import javax.persistence.Table;
 public class Location {
 
     @Id
-
+    @GeneratedValue
 
 }
