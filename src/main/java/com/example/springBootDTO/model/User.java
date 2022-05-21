25c809da725ec66965b230817500bcff85136d16
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-21 20:47:54 +0000

    User Update

--- src/main/java/com/example/springBootDTO/model/User.java
+++ src/main/java/com/example/springBootDTO/model/User.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -16,4 +14,5 @@ import javax.persistence.Table;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 }
