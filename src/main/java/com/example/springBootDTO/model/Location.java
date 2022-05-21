30528f255098c92f14cbcebabba08096d86819d2
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-21 20:58:39 +0000

    Location

--- src/main/java/com/example/springBootDTO/model/Location.java
+++ src/main/java/com/example/springBootDTO/model/Location.java
@@ -1,9 +1,11 @@
 package com.example.springBootDTO.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Location {
 }
