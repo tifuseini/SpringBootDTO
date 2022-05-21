bfffe520a8a39bcc811b7b4b0c5f27d7df79d7b8
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-21 21:00:25 +0000

    Location

--- src/main/java/com/example/springBootDTO/model/Location.java
+++ src/main/java/com/example/springBootDTO/model/Location.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -17,6 +14,6 @@ import javax.persistence.Table;
 public class Location {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
 }
