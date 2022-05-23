eb95fb015a21414431ece9efb7fff6384f8eae14
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-23 21:28:41 +0000

    User Controller

--- src/main/java/com/example/springBootDTO/restcontroller/UserController.java
+++ src/main/java/com/example/springBootDTO/restcontroller/UserController.java
@@ -1,15 +1,22 @@
 package com.example.springBootDTO.restcontroller;
 
 
+import com.example.springBootDTO.dto.UserLocationDTO;
 import com.example.springBootDTO.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/users-location")
+    public List<UserLocationDTO>  get
 
 
 }
