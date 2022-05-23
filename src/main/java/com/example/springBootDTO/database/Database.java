cffe8b6901d044f08b6842897e71c97e2e1f440e
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-23 21:38:40 +0000

    User DB

--- src/main/java/com/example/springBootDTO/database/Database.java
+++ src/main/java/com/example/springBootDTO/database/Database.java
@@ -1,6 +1,7 @@
 package com.example.springBootDTO.database;
 
 import com.example.springBootDTO.model.Location;
+import com.example.springBootDTO.model.User;
 import com.example.springBootDTO.repositories.LocationRepository;
 import com.example.springBootDTO.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
