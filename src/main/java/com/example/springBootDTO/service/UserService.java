b2f05ef1e4f038969306b6920f3a36fa3544f6b8
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-23 21:14:40 +0000

    User Service

--- src/main/java/com/example/springBootDTO/service/UserService.java
+++ src/main/java/com/example/springBootDTO/service/UserService.java
@@ -1,14 +1,19 @@
 package com.example.springBootDTO.service;
 
+import com.example.springBootDTO.dto.UserLocationDTO;
 import com.example.springBootDTO.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public List<UserLocationDTO>
 
 
 }
