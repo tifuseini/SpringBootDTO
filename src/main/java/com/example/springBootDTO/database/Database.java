8f28d96d81f4250727d24462c9dcf91c705759cd
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-23 21:49:36 +0000

    User DB

--- src/main/java/com/example/springBootDTO/database/Database.java
+++ src/main/java/com/example/springBootDTO/database/Database.java
@@ -8,36 +8,5 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class Database {
 
-    @Autowired
-    private UserRepository userRepository;
 
-    @Autowired
-    private LocationRepository locationRepository;
-
-
-    public void run(String... args) throws Exception{
-
-        Location location = new Location();
-        location.setPlace("Accra");
-        location.setDescription("Accra is expensive to live");
-        location.setLongitude(30.89);
-        location.setLatitude(40.09);
-        locationRepository.save(location);
-
-        User user1 = new User();
-        user1.setFirstName("Tahiru");
-        user1.setLastName("Iddrisu");
-        user1.setEmail("tahiru@outlook.com");
-        user1.setPassword("secret");
-        user1.setLocation(location);
-        userRepository.save(user1);
-
-        User user2 = new User();
-        user2.setFirstName("Tahiru");
-        user2.setLastName("Iddrisu");
-        user2.setEmail("tahiru@outlook.com");
-        user2.setPassword("secret");
-        user2.setLocation(location);
-        userRepository.save(user2);
-    }
 }
