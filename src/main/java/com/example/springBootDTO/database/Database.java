e867d53c3dd0502ab0a14cd1c74f83d745272635
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-23 21:33:05 +0000

    User DB

--- src/main/java/com/example/springBootDTO/database/Database.java
+++ src/main/java/com/example/springBootDTO/database/Database.java
@@ -1,8 +1,10 @@
 package com.example.springBootDTO.database;
 
+import com.example.springBootDTO.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Database {
 
     @Autowired
+    private UserRepository userRepository;
 }
